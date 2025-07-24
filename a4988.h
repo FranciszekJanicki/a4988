c2702247deb9afeb3e288dffd04456a3de9638bc
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-24 13:53:48 +0200

    changes

--- a4988.h
+++ a4988.h
@@ -15,14 +15,16 @@ a4988_err_t a4988_deinitialize(a4988_t* a4988);
 
 a4988_err_t a4988_set_frequency(a4988_t const* a4988, uint32_t frequency);
 
-a4988_err_t a4988_set_microstep(a4988_t const* a4988, a4988_microstep_t microstep);
+a4988_err_t a4988_set_microstep(a4988_t const* a4988,
+                                a4988_microstep_t microstep);
 a4988_err_t a4988_set_full_microstep(a4988_t const* a4988);
 a4988_err_t a4988_set_half_microstep(a4988_t const* a4988);
 a4988_err_t a4988_set_quarter_microstep(a4988_t const* a4988);
 a4988_err_t a4988_set_eighth_microstep(a4988_t const* a4988);
 a4988_err_t a4988_set_sixteenth_microstep(a4988_t const* a4988);
 
-a4988_err_t a4988_set_direction(a4988_t const* a4988, a4988_direction_t direction);
+a4988_err_t a4988_set_direction(a4988_t const* a4988,
+                                a4988_direction_t direction);
 a4988_err_t a4988_set_forward_direction(a4988_t const* a4988);
 a4988_err_t a4988_set_backward_direction(a4988_t const* a4988);
 a4988_err_t a4988_set_stop_direction(a4988_t const* a4988);
